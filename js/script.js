d5ebd36b4a107c43ff74104664e72925d4d342ab
fohlsom <ohlsson.fredrik@gmail.com>
2015-07-21 21:20:32 -0700

    Added a footer with Google fonts + improved the error message to clear the value and update the placeholder text.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ var markers = [];
 
 function initialize() {
 
+    getWindowHeight();
+    
+
     var mapOptions = {
         center: new google.maps.LatLng(37.6052668,-122.2342566),
         zoom: 10,
@@ -35,8 +38,6 @@ function initialize() {
 
 
     google.maps.event.addListener(autocomplete, 'place_changed', function() {
-        infowindow.close();
-        marker.setVisible(false);
         place = autocomplete.getPlace();
         
         // We remove the error class for proper results. 
@@ -46,8 +47,8 @@ function initialize() {
         if (!place.geometry) {
             console.log("Fel");
             $( ".controls" ).toggleClass("controls-error");
-            document.getElementById("pac-input").value = input.value + 
-            " Select an item from the drop-down";
+            document.getElementById("pac-input").value = "";
+            $("input").attr("placeholder", "Select a location from the drop-down list.");
             return;
         }
 
@@ -57,18 +58,6 @@ function initialize() {
         } else {
             map.setCenter(place.geometry.location);
             map.setZoom(17);  // Why 17? Because it looks good.
-        }
-
-        marker.setPosition(place.geometry.location);
-        marker.setVisible(true);
-
-        var address = '';
-        if (place.address_components) {
-            address = [
-            (place.address_components[0] && place.address_components[0].short_name || ''),
-            (place.address_components[1] && place.address_components[1].short_name || ''),
-            (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
         }
 
         var lat = place.geometry.location["A"];
@@ -125,9 +114,6 @@ function initialize() {
         }, function (error) {
             console.error("Farmersmarket list not returned. Error: ", error);
         });
-
-        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-        infowindow.open(map, marker);
     });
 }
 
@@ -186,6 +172,15 @@ function getDetails(id, marketname) {
         deferred.resolve(data, id, marketname);
     });
     return deferred.promise();
+}
+
+function getWindowHeight() {
+    $(window).resize(function () {
+        var h = $(window).height(),
+        offsetTop = 60; // Calculate the top offset
+
+        $('#map-canvas').css('height', (h - offsetTop));
+    }).resize();
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
